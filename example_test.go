4eb5beb8af797168da58444cc6c356573e5dde2a
Coda Hale <coda.hale@gmail.com>
2026-08-07 20:54:37 -0600

    Add external examples for Hash and XOF
    Compile the public constructors and standard hash interfaces from an external package. Exercise fixed digest output and continued XOF reads against RFC test vectors.

---
+++ example_test.go
@@ -0,0 +1,39 @@
+package kt128_test
+
+import (
+	"fmt"
+	"hash"
+
+	"github.com/codahale/kt128"
+)
+
+var (
+	_ hash.Hash   = (*kt128.Hash)(nil)
+	_ hash.Cloner = (*kt128.Hash)(nil)
+	_ hash.XOF    = (*kt128.XOF)(nil)
+)
+
+func ExampleNewHash() {
+	h := kt128.NewHash(nil, 32)
+	_, _ = h.Write([]byte{0})
+	fmt.Printf("%x\n", h.Sum(nil))
+
+	// Output:
+	// 2bda92450e8b147f8a7cb629e784a058efca7cf7d8218e02d345dfaa65244a1f
+}
+
+func ExampleNewXOF() {
+	x := kt128.NewXOF(nil)
+	_, _ = x.Write([]byte{0})
+
+	var first, second [16]byte
+	_, _ = x.Read(first[:])
+	_, _ = x.Read(second[:])
+
+	fmt.Printf("%x\n", first)
+	fmt.Printf("%x\n", second)
+
+	// Output:
+	// 2bda92450e8b147f8a7cb629e784a058
+	// efca7cf7d8218e02d345dfaa65244a1f
+}
